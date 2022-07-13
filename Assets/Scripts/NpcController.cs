10670bb574ddfd1fd2fedb0078085e456fd2027e
Shivan G <101424881+ShivanDavid@users.noreply.github.com>
2022-07-13 03:24:31 +0200

    NpcController AddForce

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -7,6 +7,8 @@ public class NpcController : MonoBehaviour
     public NPC npc;
     public SpriteRenderer sprite;
 
+    public new Rigidbody2D rigidbody;
+
     public GameObject target;
     private Vector2 movementDirection;
 
@@ -17,6 +19,7 @@ public class NpcController : MonoBehaviour
 
     private void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         InitNPC();
     }
 
@@ -45,6 +48,33 @@ public class NpcController : MonoBehaviour
     {
         target.GetComponent<CharackterController>().currentHealth -=  this.damage;
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.CompareTag("NPC"))
+        {
+            Debug.Log("1");
+            if (collision.transform.position.y < gameObject.transform.position.y)
+            {
+                Debug.Log("2");
+                rigidbody.AddForce(Vector2.up * 100f);
+            }
+            else
+            {
+                rigidbody.AddForce(Vector2.down * 100f);
+            }
+            if (collision.transform.position.x < gameObject.transform.position.x)
+            {
+                rigidbody.AddForce(Vector2.right * 100f);
+            }
+            else
+            {
+                rigidbody.AddForce(Vector2.left * 100f);
+            }
+        }
+    }
+
 
     public void DestroyNPC()
     {
