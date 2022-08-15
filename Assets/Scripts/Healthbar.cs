fc5da44984143b703603bc1b197d7abc47e40392
Shivan G <101424881+ShivanDavid@users.noreply.github.com>
2022-08-15 02:29:06 +0200

    Fix Weapon Attack distance & HP Numbers
    Die Distanz des Angriffs exakter gemacht
    Lebensbalken haben jetzt auch die HP Anzahl darin

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Healthbar : MonoBehaviour
 {
@@ -10,10 +11,24 @@ public class Healthbar : MonoBehaviour
     public Gradient gradient;
     public Image fill;
 
+    public TMP_Text text;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        text.SetText(health.ToString());
 
         fill.color = gradient.Evaluate(1f);
     }
@@ -21,6 +36,8 @@ public class Healthbar : MonoBehaviour
     public void SetHealth(int health)
     {
         slider.value = health;
+
+        text.SetText(health.ToString());
 
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
